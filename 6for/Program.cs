059d67a3ccb6b4f93b54aad417ade96c5b7d4d42
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-10 09:40:55 +0200

    Add project files.

---
+++ 6for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int total = 0;
+            Console.WriteLine("Program oblicza sumę pięciu liczb.");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Wprowadź liczbę: ");
+                number = int.Parse(Console.ReadLine());
+                total = total + number;
+            }
+            Console.WriteLine($"Suma jest równa {total}");
+        }
+    }
+}
